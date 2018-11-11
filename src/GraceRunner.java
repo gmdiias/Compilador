c4914b1bb7d50e2c1be1b680d288e3b7445834ec
Gustavo Mendonça Dias <gmdiias@gmail.com>
2018-11-11 10:36:10 -0200

    comecando a linguagem semantica

--- src/GraceRunner.java
+++ src/GraceRunner.java
@@ -1,29 +1,25 @@
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class GraceRunner {
 	public static void main(String[] args) throws Exception {
-
-		/*ANTLRInputStream input = new ANTLRInputStream(System.in);
-
-		HelloLexer lexer = new HelloLexer(input);
-
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-
-		HelloParser parser = new HelloParser(tokens);
-		ParseTree tree = parser.if_token(); // begin parsing at rule 'r'
-		System.out.println(tree.toStringTree(parser)); // print LISP-style tree*/
 		 
-	    ANTLRInputStream input = new ANTLRInputStream(System.in);
+	    ANTLRInputStream input = new ANTLRInputStream("var a : int;");
 		
 	    GraceLexer lexer = new GraceLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 
 		GraceParser parser = new GraceParser(tokens);
 		
-		parser.decVar();
-		//ParseTree tree = parser.grace();
-		//System.out.println(tree.toStringTree(parser)); // print LISP-style tree
+		ParseTree tree = parser.decVar();
+		
+		GraceBaseListener listener = new GraceBaseListener();
+		
+		ParseTreeWalker walker = new ParseTreeWalker();
+		
+		walker.walk(listener, tree);
+		
 	}
 }
